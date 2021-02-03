8428bfee9e706ccab237ce6b2e591dc59918f493
Laura <lkzailac@gmail.com>
2021-02-03 16:26:43 -0500

    Phase 1B

--- spud-face.js
+++ spud-face.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-  // ** Phase 1B: Update license with event delegation and event.target ** 
+  // ** Phase 1B: Update license with event delegation and event.target **
   const submitButton = document.getElementsByClassName("form__submit");
-  const userInput = document.getElementById('title');
+  //const userInput = document.getElementById('title');
   // console.log('userInput:', userInput)
 
-  const licenseEl = document.getElementById('card-title')
+
   // console.log('licenseEl:', licenseEl)
 
   submitButton.addEventListener("submit", (event) => {
@@ -13,9 +13,23 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const licenseIdCompVal = document.getElementById("license-num-confirm").value;
     if (licenseId1Val === licenseIdCompVal) {
 
-    }
-  });
+      function updateLicense(inputId, OutputId) {
+        const userInput = document.getElementById(inputId).value;
 
+        squareDiv.setAttribute("class", "square");
+        squareDiv.style.backgroundColor = color;
+        liElement.append(squareDiv);
+
+      }
+      outputId.value = inputId.value;
+
+
+    }
+
+  });
+  updateLicense("blue-item", "blue");
+  updateLicense("red-item", "red");
+  updateLicense("purple-item", "purple");
 
   // ** Phase 2: Add focus and blur events to form inputs **
 
