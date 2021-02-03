ba509155c1d3dc8483f8bd83caff086cd91267a9
Andrew Moss <andrewmoss@Andrews-MacBook-Pro.local>
2021-02-03 15:06:57 -0600

    phase1 -am

--- spud-face.js
+++ spud-face.js
@@ -1,15 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
   // ** Phase 1B: Update license with event delegation and event.target ** 
+  const submitButton = document.getElementsByClassName("form__submit");
+  const userInput = document.getElementById('title');
+  // console.log('userInput:', userInput)
+
+  const licenseEl = document.getElementById('card-title')
+  // console.log('licenseEl:', licenseEl)
+
+  submitButton.addEventListener("submit", (event) => {
+    const licenseId1Val = document.getElementById("license-num").value;
+    const licenseIdCompVal = document.getElementById("license-num-confirm").value;
+    if (licenseId1Val === licenseIdCompVal) {
+
+    }
+  });
 
 
   // ** Phase 2: Add focus and blur events to form inputs **
- 
+
+
+
 
   // ** Phase 3: Check that license numbers match **
- 
+
+
+
 
   // ** Phase 4: Update submit button click count **
 
- 
+
+
+
 });
